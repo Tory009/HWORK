6661ca2065c1e0bc966a2e01065b0eabd9df088f
Tory009 <148058802+Tory009@users.noreply.github.com>
2024-06-25 02:03:57 +0500

    add lesson 80

---
+++ Lesson 80/main.js
@@ -0,0 +1,21 @@
+for (let a = 2; a <= 12; a = a + 2) {
+  console.log(a)
+}
+for (let k = 10; k >= 1; k = k - 1) {
+console.log(k)
+}
+
+let i = 0 
+
+while(i < 12) {
+  i = i + 2
+  console.log(i)
+}
+
+let p = 10
+
+while(p >= 1) {
+
+  console.log(p)
+  p = p - 1
+}
