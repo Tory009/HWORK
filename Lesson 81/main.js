ef80265d8c8d8f775a3ef5686549aef6f02ec126
Tory009 <148058802+Tory009@users.noreply.github.com>
2024-06-25 02:09:17 +0500

    add lesson 81

---
+++ Lesson 81/main.js
@@ -0,0 +1,13 @@
+
+const arr = ['красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый']
+
+for (let i = 7; i >= 0; i-- ) {
+ console.log(arr[i])
+}
+
+const number = [8, 140, 15, 13, 18, 288, 350, 1024, 50]
+
+for (let i = 0; i <= number.length; i++) {
+  if(number[i] % 2 == 0)
+    console.log(number[i])
+}
