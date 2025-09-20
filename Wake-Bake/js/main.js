60574341fd1e76dc74e684c0b2e2df3b2024497d
Tory009 <148058802+Tory009@users.noreply.github.com>
2025-09-20 15:24:19 +0500

    add template

--- Wake-Bake/js/main.js
+++ Wake-Bake/js/main.js
@@ -93,7 +93,37 @@
     })
   })
 
+   // Слайдер-галерея
 
+  const swiper = new Swiper('.gallery__slider', {
+
+  spaceBetween: 15,
+  slidesPerView: 1.5,
+
+  pagination: {
+    el: '.gallery__pagination',
+    type: 'fraction'
+  },
+
+
+  navigation: {
+    nextEl: '.gallery__next',
+    prevEl: '.gallery__prev',
+  },
+  breakpoints: {
+    601: {
+      spaceBetween: 32,
+      slidesPerView: 3,
+    },
+    801: {
+      spaceBetween: 32,
+      slidesPerView: 4,
+    },
+    1101: {
+      slidesPerView: 4,
+    }
+  }
+});
 
 
 })()
