3fa8c28f1cc7f080933c6011e8da3e56ff56cd25
Tory009 <148058802+Tory009@users.noreply.github.com>
2025-10-21 18:15:00 +0500

    add

--- Wake-Bake/js/main.js
+++ Wake-Bake/js/main.js
@@ -95,7 +95,7 @@
 
    // Слайдер-галерея
 
-  const swiper = new Swiper('.gallery__slider', {
+  new Swiper('.gallery__slider', {
 
   spaceBetween: 15,
   slidesPerView: 1.5,
@@ -121,6 +121,33 @@
     },
     1101: {
       slidesPerView: 4,
+    }
+  }
+});
+// Слайдер-отзывы
+    new Swiper('.testimonials__slider', {
+
+  spaceBetween: 0,
+  slidesPerView: 1,
+  centeredSlides: true,
+
+  navigation: {
+    nextEl: '.testimonials__next',
+    prevEl: '.testimonials__prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
+
+    breakpoints: {
+    901: {
+      spaceBetween: 32,
+      slidesPerView: 1.5,
+    },
+    1201: {
+      slidesPerView: 2.1,
     }
   }
 });
